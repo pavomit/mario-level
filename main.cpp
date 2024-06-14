00652c42c8d1c279c66d9d932b614c9d6f647860
pavomit <spiderocta@proton.me>
2024-06-14 14:50:25 +0300

    added assets and initial sprite

--- main.cpp
+++ main.cpp
@@ -1,22 +1,37 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(500, 500), "Mario Level");
+
+    sf::Texture marioTexture;
+    if (!marioTexture.loadFromFile("../assets/images/mario.png")) {
+        std::cerr << "Error loading mario texture @assets/images\n";
+    }
+
+    sf::Sprite marioSprite(marioTexture);
+    marioSprite.setScale(0.5, 0.5);
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
                 window.close();
+
+            // moving mario
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+                marioSprite.move(5, 0);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+                marioSprite.move(-5, 0);
+            }
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(marioSprite);
         window.display();
     }
 
