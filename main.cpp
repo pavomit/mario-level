95b51c64b7d9df59a8e30e1dd6589a3a08110477
pavomit <spiderocta@proton.me>
2024-06-16 14:39:05 +0300

    jumping and gravity

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
 {
     //++++++++++++++++++++ window +++++++++++++++++++++++++++++
     sf::RenderWindow window(sf::VideoMode(1200, 700), "Mario Level");
-    window.setFramerateLimit(60);
+    window.setFramerateLimit(120);
 
     //+++++++++++++++++++ mario +++++++++++++++++++++++++++++++++
     sf::Texture marioTexture;
@@ -15,7 +15,7 @@ int main()
 
     sf::Sprite marioSprite(marioTexture);
     marioSprite.setScale(0.5, 0.5);
-    marioSprite.setPosition(100, 500);
+    marioSprite.setPosition(100, 533);
 
     //++++++++++++++++++++++ background ++++++++++++++++++++++++
     sf::Texture backgroundTexture;
@@ -43,38 +43,58 @@ int main()
     // no blocks(holes)
     blockSprites[7].setScale(0, 0);
 
-    //++++++++++++++ animation ++++++++++++++++++++++++++++++++
+    //++++++++++++++ animation realted variables ++++++++++++++++++++++++++++++++
     int animation_state{ 0 };
     float timer{ 0 };
+    //++++++++++++++ gravity related varibles ++++++++++++++++++++++++++++++++
+    float xForce{}, yForce{};
 
     while (window.isOpen())
     {
         timer += 0.1;
         marioSprite.setTextureRect(sf::IntRect(animation_state, 0, 91, 175));
         sf::Event event;
+        xForce = 0;
+        if (yForce <= 5) {
+            yForce += 0.1;
+        }
+        //collision with blocks after jump
+        for (int i = 0; i < numberOfBlocks; i++)
+        {
+            if (marioSprite.getGlobalBounds().intersects(blockSprites[i].getGlobalBounds())) {
+                yForce = 0;
+            }
+        }
+
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
                 window.close();
+        }
 
-            // moving mario
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-                marioSprite.move(5, 0);
-                marioSprite.setScale(0.5, 0.5);
-                if (timer > 1) {
-                    animation_state += 91;
-                    timer = 0;
-                }
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-                marioSprite.move(-5, 0);
-                marioSprite.setScale(-0.5, 0.5);
-                if (timer > 1) {
-                    animation_state += 91;
-                    timer = 0;
-                }
+        // moving mario
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            xForce = 3;
+            marioSprite.setScale(0.5, 0.5);
+            if (timer > 1) {
+                animation_state += 91;
+                timer = 0;
             }
         }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            xForce = -3;
+            marioSprite.setScale(-0.5, 0.5);
+            if (timer > 1) {
+                animation_state += 91;
+                timer = 0;
+            }
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && yForce == 0) {
+            yForce = -5;
+        }
+
+        marioSprite.move(xForce, yForce);
 
         if (animation_state > 182) {
             animation_state = 0;
