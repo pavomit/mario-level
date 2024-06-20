38276d8da936a0a117dfb75e333d13cb79998d69
pavomit <spiderocta@proton.me>
2024-06-20 19:38:43 +0300

    defined basic game states

--- main.cpp
+++ main.cpp
@@ -41,7 +41,7 @@ int main()
     }
     sf::Sprite enemySprite(enemyTexture);
     enemySprite.setScale(0.3, 0.3);
-    enemySprite.setPosition(400, 570);
+    enemySprite.setPosition(650, 570);
 
     bool emenyDirection = true; // true - > right | false <- left
 
@@ -78,89 +78,123 @@ int main()
     camera.setSize(1200, 700);
     camera.setCenter(600, 350);
 
+    //++++++++++++++ game phases +++++++++++++++++++++++++
+    /*
+    * 1- Mian Menu
+    * 2- Play
+    * 3- Win
+    * 4- Lose 
+    */
+    int phase = 2;
+
     while (window.isOpen())
     {
-        timer += 0.1;
-        marioSprite.setTextureRect(sf::IntRect(animation_state, 0, 91, 175));
-        sf::Event event;
-        xForce = 0;
-        if (yForce <= 5) {
-            yForce += 0.1;
-        }
-        
-        //enemy movement
+        // play
+        if (phase == 2) {
+            // pahse:2 - start
+            timer += 0.1;
+            marioSprite.setTextureRect(sf::IntRect(animation_state, 0, 91, 175));
+            sf::Event event;
+            xForce = 0;
 
-        if (enemySprite.getPosition().x <= 600) {
-            emenyDirection = true;
-        }
-        else if (enemySprite.getPosition().x >= 1000) {
-            emenyDirection = false;
-        }
-
-        if (emenyDirection) {
-            enemySprite.move(2, 0);
-        }
-        else {
-            enemySprite.move(-2, 0);
-        }
-
-        //collision with blocks after jump
-        for (int i = 0; i < numberOfBlocks; i++)
-        {
-            if (marioSprite.getGlobalBounds().intersects(blockSprites[i].getGlobalBounds())) {
-                yForce = 0;
+            // state conditions
+            if (marioSprite.getGlobalBounds().intersects(houseSprite.getGlobalBounds())) {
+                phase = 3;
+                continue;
             }
-        }
-
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-                window.close();
-        }
-
-        // moving mario
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-            xForce = 3;
-            marioSprite.setScale(0.5, 0.5);
-            if (timer > 1) {
-                animation_state += 91;
-                timer = 0;
+            if (marioSprite.getGlobalBounds().intersects(enemySprite.getGlobalBounds())) {
+                phase = 4;
+                continue;
             }
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && marioSprite.getPosition().x > 100) {
-            xForce = -3;
-            marioSprite.setScale(-0.5, 0.5);
-            if (timer > 1) {
-                animation_state += 91;
-                timer = 0;
+
+            if (yForce <= 5) {
+                yForce += 0.1;
             }
+
+            //enemy movement
+            if (enemySprite.getPosition().x <= 620) {
+                emenyDirection = true;
+            }
+            else if (enemySprite.getPosition().x >= 940) {
+                emenyDirection = false;
+            }
+
+            if (emenyDirection) {
+                enemySprite.move(2, 0);
+            }
+            else {
+                enemySprite.move(-2, 0);
+            }
+
+            //collision with blocks after jump
+            for (int i = 0; i < numberOfBlocks; i++)
+            {
+                if (marioSprite.getGlobalBounds().intersects(blockSprites[i].getGlobalBounds())) {
+                    yForce = 0;
+                }
+            }
+
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+                    window.close();
+            }
+
+            // moving mario
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+                xForce = 3;
+                marioSprite.setScale(0.5, 0.5);
+                if (timer > 1) {
+                    animation_state += 91;
+                    timer = 0;
+                }
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && marioSprite.getPosition().x > 100) {
+                xForce = -3;
+                marioSprite.setScale(-0.5, 0.5);
+                if (timer > 1) {
+                    animation_state += 91;
+                    timer = 0;
+                }
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && yForce == 0) {
+                yForce = -5;
+            }
+
+            marioSprite.move(xForce, yForce);
+
+            if (animation_state > 182) {
+                animation_state = 0;
+            }
+
+            //static background inside camera
+            backgroundSprite.setPosition(marioSprite.getPosition().x - 100, 0);
+
+            camera.setCenter(marioSprite.getPosition().x + 500, 350);
+            window.setView(camera);
+
+            window.clear();
+            window.draw(backgroundSprite);
+            window.draw(houseSprite);
+            window.draw(enemySprite);
+            for (int i = 0; i < numberOfBlocks; i++) {
+                window.draw(blockSprites[i]);
+            }
+            window.draw(marioSprite);
+            window.display();
+            // pahse:2 - end
         }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && yForce == 0) {
-            yForce = -5;
+        // win
+        if (phase == 3) {
+            window.clear(sf::Color::Green);
+            window.display();
         }
-
-        marioSprite.move(xForce, yForce);
-
-        if (animation_state > 182) {
-            animation_state = 0;
+        // lose
+        if (phase == 4) {
+            window.clear(sf::Color::Red);
+            window.display();
         }
-
-        //static background inside camera
-        backgroundSprite.setPosition(marioSprite.getPosition().x - 100, 0);
-
-        camera.setCenter(marioSprite.getPosition().x + 500, 350);
-        window.setView(camera);
-
-        window.clear();
-        window.draw(backgroundSprite);
-        window.draw(houseSprite);
-        window.draw(enemySprite);
-        for (int i = 0; i < numberOfBlocks; i++) {
-            window.draw(blockSprites[i]);
-        }
-        window.draw(marioSprite);
-        window.display();
     }
 
     return 0;
